f040c44b140ea82daf47962dddb6432846cf3a5a
Альфия Абирова <alfa666samara@gmail.com>
2023-06-17 00:09:40 +0300

    Пробный коммит, первое ДЗ

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,19 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = Math.pow(b, 2)-4*a*c;
+  if(d < 0){
+    return arr;
+  }
+  else if(d === 0){
+    let x = -b/(2*a);
+    arr.push(x);
+  }
+  else if(d > 0){
+    let x = (-b + Math.sqrt(d) )/(2*a);
+    let y = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(x, y);
+  }
   
   return arr;
 }
