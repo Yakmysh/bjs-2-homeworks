56efc66f5ece759449181769272334bb3267d815
Альфия Абирова <alfa666samara@gmail.com>
2023-06-17 01:02:04 +0300

    Второй пробный запуск, неоконченная ДЗ-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,19 +5,23 @@ function solveEquation(a, b, c) {
   if(d < 0){
     return arr;
   }
-  else if(d === 0){
-    let x = -b/(2*a);
-    arr.push(x);
-  }
   else if(d > 0){
     let x = (-b + Math.sqrt(d) )/(2*a);
     let y = (-b - Math.sqrt(d) )/(2*a);
     arr.push(x, y);
+  }
+  else if(d === 0){
+    let x = -b/(2*a);
+    arr.push(x);
   }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percent = (percent / 100) /12; 
+  let credit = amount - contribution;  
+  let monthlyInstallment = credit * (percent + (percent / (((1 + percent)** countMonths)-1)));
+  let sum = (monthlyInstallment * countMonths)* percent;
+  return sum;
 }
