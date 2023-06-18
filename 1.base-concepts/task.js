255c6ca01275ae2bbe940e73faf8cfed432d7253
Альфия Абирова <alfa666samara@gmail.com>
2023-06-18 20:48:22 +0300

    Последнее преображение ДЗ-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,23 +5,23 @@ function solveEquation(a, b, c) {
   if(d < 0){
     return arr;
   }
+  else if(d === 0){
+    let x = -b/(2*a);
+    arr.push(x);
+  }
   else if(d > 0){
     let x = (-b + Math.sqrt(d) )/(2*a);
     let y = (-b - Math.sqrt(d) )/(2*a);
     arr.push(x, y);
   }
-  else if(d === 0){
-    let x = -b/(2*a);
-    arr.push(x);
-  }
-  
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percent = (percent / 100) /12; 
+  let percent = (((1 + percent / 100) * 1/12 - 1) * 0.1);
   let credit = amount - contribution;  
   let monthlyInstallment = credit * (percent + (percent / (((1 + percent)** countMonths)-1)));
-  let sum = (monthlyInstallment * countMonths)* percent;
-  return sum;
+  let sum = + (monthlyInstallment * countMonths).toFixed(2);
+  return sum; 
+
 }
