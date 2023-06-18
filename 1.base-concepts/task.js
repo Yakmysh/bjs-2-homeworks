797d042c1de0f0b0f42b76bcd4d8916367081b0e
Альфия Абирова <alfa666samara@gmail.com>
2023-06-18 21:46:01 +0300

    Самое последнее исправление

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,28 @@
 "use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = Math.pow(b, 2)-4*a*c;
-  if(d < 0){
-    return arr;
-  }
-  else if(d === 0){
-    let x = -b/(2*a);
-    arr.push(x);
-  }
-  else if(d > 0){
-    let x = (-b + Math.sqrt(d) )/(2*a);
-    let y = (-b - Math.sqrt(d) )/(2*a);
-    arr.push(x, y);
-  }
-  return arr;
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
+	if (d > 0) {
+		let x = (-b + Math.sqrt(d)) / (2 * a);
+		let y = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x, y);
+	} else if (d === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+	} else if (d < 0) {
+		return arr;
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percent = (((1 + percent / 100) * 1/12 - 1) * 0.1);
-  let credit = amount - contribution;  
-  let monthlyInstallment = credit * (percent + (percent / (((1 + percent)** countMonths)-1)));
-  let sum = + (monthlyInstallment * countMonths).toFixed(2);
-  return sum; 
+	let p = percent / 100 / 12;
+	let s = amount - contribution;
+  let n = countMonths;
+	let monthlyInstallment = s * (p + (p / (((1 + p) ** n) - 1)));
+	let sum = +(monthlyInstallment * countMonths).toFixed(2);
+	return sum;
 
 }
