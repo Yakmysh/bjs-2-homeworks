aac7f4a05dee0f5680657f2f0032569815c815cf
Альфия Абирова <alfa666samara@gmail.com>
2023-06-28 02:00:31 +0300

    Работа исполнена

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -11,47 +11,54 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-	return sum.reduce(...arr);
+	return summ = [...arr].reduce((item, arr) => item + arr, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-	return Math.max(... arr) - Math.min(... arr);
+	if (!arr.length){
+		return 0;
+	}
+	return result = (Math.max(... arr) - Math.min(... arr));
 }
 
 function differenceEvenOddWorker(...arr) {
 	let sumEvenElement = 0;
 	let sumOddElement = 0;
-	let sum = sumEvenElement - sumOddElement;
+	if (!arr.length) {
+		return 0;
+	}
 	for (let i = 0; i < arr.length; i++) {
-		if (i = 2) {
-			sumEvenElement + 1;
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
 		} else {
-			sumOddElement + 1;
+			sumOddElement += arr[i];
 		}
 	}
-	return sum;
+	return sumEvenElement - sumOddElement;
 }
- 
 
 function averageEvenElementsWorker(...arr) {
-  let sumEvenElement = 0;
-  let countEvenElement = 0;
-	for (let i = 0; i < arr.length; i++) {
-	  if (i == 2) {
-	   sumEvenElement += [i];
-	   countEvenElement += 1;
-	  }
-	  return (sumEvenElement / countEvenElement);
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	if (!arr.length){
+		return 0;
 	}
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+			countEvenElement += 1;
+		}
+	}
+	return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
-  let maxWorkerResult = -Infinity;
-    for (let i = 0; i < arrOfArr.length; i++){
-		let result = func(arrOfArr[i]);
-      if (result > maxWorkerResult){
-		maxWorkerResult = result;
-	  }
-  }
-  return axWorkerResult;  
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = - Infinity;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		let result = func(...arrOfArr[i]);
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
+		}
+	}
+	return maxWorkerResult;
 }
