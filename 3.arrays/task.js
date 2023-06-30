d205b33db9d968113584c5a0ee4602e631f21dd2
Альфия Абирова <alfa666samara@gmail.com>
2023-06-30 21:01:40 +0300

    Наконец-то решено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,27 @@
 function compareArrays(arr1, arr2) {
-  
+	let a = arr1.filter(element => element.arr1 === element.arr2);
+  let b = arr1.every((value, index) => value === arr2[index]);
+  if (arr1.length === arr2.length && b === true) {
+		return true;
+	} else if (arr1.length !== arr2.length && b !== true) {
+    return false;
+  }
+  else if (a !== b){
+		return false;
+	} else {
+		return false;
+	}
 }
+compareArrays([3, 2, 1], [3, 2, 1]);
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let client = users.filter(user => user.gender === gender);
+  let clientAge = client.map(user => user.age);
+  if (!users.length || client == 0){
+    return 0;
+  }
+  else {
+    let sumAge = clientAge.reduce((acc, age) => acc + age) / clientAge.length;
+    return sumAge;
+  }
 }
